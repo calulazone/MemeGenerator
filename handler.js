fd8cfe4d39da15c69ea493cceea540bcca58c232
calulazone <lutinelo.lg@gmail.com>
2025-03-05 16:53:46 +0100

    meme generator

--- handler.js
+++ handler.js
@@ -1,5 +1,16 @@
-const AWS = require("aws-sdk");
+const axios = require('axios');
+const sharp = require('sharp');
 const { v4: uuidv4 } = require("uuid");
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  endpoint: `http://${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}`,
+  accessKeyId: process.env.MINIO_ACCESS_KEY,
+  secretAccessKey: process.env.MINIO_SECRET_KEY,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+});
+
+const BUCKET_NAME = process.env.MINIO_BUCKET;
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient(
   process.env.IS_OFFLINE && {
@@ -10,38 +21,102 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient(
 
 const TABLE_NAME = process.env.DYNAMODB_TABLE;
 
-exports.index = async (event) => {
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-      message: "Url Shortener v1.0",
-    }),
-  };
+module.exports.generateMeme = async (event) => {
+  console.log('Requête reçu:', event);
+  const body = JSON.parse(event.body);
+  const { imageUrl, topText, bottomText } = body;
+
+  if (!imageUrl) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Image URL requise' }),
+    };
+  }
+
+  try {
+    const response = await axios({
+      method: 'get',
+      url: imageUrl,
+      responseType: 'arraybuffer',
+    });
+
+    console.log('Création du meme...');
+    const maxWidth = 1200;
+    let image = sharp(response.data);
+    
+    const metadata = await image.metadata();
+    const width = metadata.width;
+    
+    if (width > maxWidth) {
+      image = image.resize(maxWidth);
+    }
+
+    const memeImage = await sharp(response.data)
+    .composite([
+      {
+        input: Buffer.from(`
+          <svg width="100%" height="100%">
+            <text x="50%" y="90%" font-size="100" text-anchor="middle" fill="white" font-family="Arial, sans-serif">${topText}</text>
+          </svg>`),
+        top: 30,
+        left: 10
+      },
+      {
+        input: Buffer.from(`
+          <svg width="100%" height="100%">
+            <text x="50%" y="90%" font-size="100" text-anchor="middle" fill="white" font-family="Arial, sans-serif">${bottomText}</text>
+          </svg>`),
+        top: 500,
+        left: 10
+      }
+    ])
+    .toBuffer();
+
+    console.log('Meme créé. Envoi vers s3...'); 
+
+    const newImageName = `generated-${Date.now()}-${imageUrl.split('/').pop()}`;
+    const uploadParams = {
+      Bucket: BUCKET_NAME,
+      Key: newImageName,
+      Body: memeImage,
+      ContentType: 'image/png',
+    };
+
+    await s3.putObject(uploadParams).promise();
+    console.log(`http://${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}/${BUCKET_NAME}/${newImageName}`)
+
+    const url = `http://${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}/${BUCKET_NAME}/${newImageName}`
+    const key = uuidv4().slice(0, 8);
+  
+    const params = {
+      TableName: TABLE_NAME,
+      Item: {
+        key,
+        url,
+        createdAt: new Date().toISOString(),
+      },
+    };
+  
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Meme généré avec succès',
+        memeUrl: url,
+      }),
+    };
+  } catch (error) {
+    console.error('Erreur.', error.message || error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Erreur durant la génération du meme', error: error.message || error }),
+    };
+  }
 };
 
-exports.createUrl = async (event) => {
-  const { url } = JSON.parse(event.body);
-  const key = uuidv4().slice(0, 8);
 
-  const params = {
-    TableName: TABLE_NAME,
-    Item: {
-      key,
-      url,
-      createdAt: new Date().toISOString(),
-      clicks: 0,
-    },
-  };
-
-  await dynamoDb.put(params).promise();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ key, shortUrl: `http://localhost:3000/url/${key}` }),
-  };
-};
-
-exports.listUrls = async (event) => {
+exports.listMemesUrl = async (event) => {
   const params = {
     TableName: TABLE_NAME,
   };
@@ -51,46 +126,5 @@ exports.listUrls = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(Items),
-  };
-};
-
-exports.getUrl = async (event) => {
-  const { key } = event.pathParameters;
-
-  const params = {
-    TableName: TABLE_NAME,
-    Key: {
-      key,
-    },
-  };
-
-  const { Item } = await dynamoDb.get(params).promise();
-
-  // Increase the number of clicks
-  await dynamoDb
-    .update({
-      TableName: TABLE_NAME,
-      Key: {
-        key,
-      },
-      UpdateExpression: "SET clicks = clicks + :inc",
-      ExpressionAttributeValues: {
-        ":inc": 1,
-      },
-    })
-    .promise();
-
-  if (!Item) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ message: "URL not found" }),
-    };
-  }
-
-  return {
-    statusCode: 301,
-    headers: {
-      Location: Item.url,
-    },
   };
 };
